1b2a0f8c329fa3075f8d04ae3066541419abba22
vijeyash1 <vijeyash@gmail.com>
2022-05-19 22:54:36 +0530

    modified github agent

--- githubagent/github_agent.go
+++ githubagent/github_agent.go
@@ -12,27 +12,41 @@ const (
 	path = "/webhooks"
 )
 
-func main() {
+func event(w http.ResponseWriter, r *http.Request) {
 	hook, _ := github.New(github.Options.Secret("helloworld"))
+	payload, err := hook.Parse(r, github.PushEvent)
+	//fmt.Printf("%T \n", payload)
 
-	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
-		payload, err := hook.Parse(r, github.PushEvent)
-
-		if err != nil {
-			if err == github.ErrEventNotFound {
-				// ok event wasn;t one of the ones asked to be parsed
-				fmt.Println("this event was not present")
-			}
+	if err != nil {
+		if err == github.ErrEventNotFound {
+			// ok event wasn;t one of the ones asked to be parsed
+			fmt.Println("this event was not present")
 		}
-		switch payload.(type) {
+	}
+	switch payload.(type) {
 
-		case github.ReleasePayload:
-			release := payload.(github.PushPayload)
-			// Do whatever you want from here...
-			//fmt.Printf("%+v", release)
-			fmt.Printf("%s", release.Pusher.Name)
-		}
-	})
+	case github.PushPayload:
+		release := payload.(github.PushPayload)
+		// Do whatever you want from here...
+		//fmt.Printf("%+v", release)
+		//fmt.Println("data")
+
+		// for _, val := range release.Commits {
+		// 	for _, v := range val.Modified {
+		// 		fmt.Printf("added: %s \n", v)
+		// 	}
+		// 	fmt.Printf("author name: %s \n", val.Author.Name)
+		// }
+		fmt.Printf("commited by: %s \n", release.Commits[0].Author.Name)
+		fmt.Printf("commited at: %s \n", release.HeadCommit.Timestamp)
+		fmt.Printf("repository name:%s \n", release.Repository.Name)
+
+	}
+}
+
+func main() {
+
+	http.HandleFunc(path, event)
 	fmt.Println("github webhook server started at port 8000")
 	http.ListenAndServe(":8000", nil)
 }
